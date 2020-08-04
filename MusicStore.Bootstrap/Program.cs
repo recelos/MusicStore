2d42a1630a0aa5a8ba106b3897f081563f8181ea
Exodus <kubagr2000@gmail.com>
2020-08-05 00:24:23 +0200

    Add project files.

---
+++ MusicStore.Bootstrap/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MusicStore.Bootstrap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
